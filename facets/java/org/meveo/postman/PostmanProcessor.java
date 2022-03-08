1347e15124a5d4ffec97c81677101fb719ef399a
meveo.admin <no-mail@meveo.com>
2022-03-08 08:59:01 +0000

    Add the script File : org.meveo.postman.PostmanProcessorin the module : mv-postman

---
+++ facets/java/org/meveo/postman/PostmanProcessor.java
@@ -0,0 +1,15 @@
+package org.meveo.postman;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class PostmanProcessor extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
