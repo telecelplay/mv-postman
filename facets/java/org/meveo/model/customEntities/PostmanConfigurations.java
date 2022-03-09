14b0447f2d999b9dae16b9b909a625221863609b
meveo.admin <no-mail@meveo.com>
2022-03-09 20:41:43 +0000

    Add the cet java source file : PostmanConfigurations.javain the module : mv-postman

--- facets/java/org/meveo/model/customEntities/PostmanConfigurations.java
+++ facets/java/org/meveo/model/customEntities/PostmanConfigurations.java
@@ -3,9 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.util.Map;
-import java.util.HashMap;
-import org.meveo.admin.job.FileProcessingJob;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class PostmanConfigurations implements CustomEntity {
@@ -22,18 +19,6 @@ public class PostmanConfigurations implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String fileName;
-
-    private String response;
-
-    private Map<String, String> context = new HashMap<>();
-
-    private Long failedTests;
-
-    private FileProcessingJob testField;
-
-    private Long totalTests;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -49,54 +34,6 @@ public class PostmanConfigurations implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
-
-    public Map<String, String> getContext() {
-        return context;
-    }
-
-    public void setContext(Map<String, String> context) {
-        this.context = context;
-    }
-
-    public Long getFailedTests() {
-        return failedTests;
-    }
-
-    public void setFailedTests(Long failedTests) {
-        this.failedTests = failedTests;
-    }
-
-    public FileProcessingJob getTestField() {
-        return testField;
-    }
-
-    public void setTestField(FileProcessingJob testField) {
-        this.testField = testField;
-    }
-
-    public Long getTotalTests() {
-        return totalTests;
-    }
-
-    public void setTotalTests(Long totalTests) {
-        this.totalTests = totalTests;
     }
 
     @Override()
