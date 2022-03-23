0dc71455d7d1c9cc36c54d771406d2beff5b069f
meveo.admin <no-mail@meveo.com>
2022-03-23 19:33:23 +0000

    Add the cet java source file : PostmanProcessor.javain the module : mv-postman

---
+++ facets/java/org/meveo/model/customEntities/PostmanProcessor.java
@@ -0,0 +1,94 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class PostmanProcessor implements CustomEntity {
+
+    public PostmanProcessor() {
+    }
+
+    public PostmanProcessor(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private Long failedTest;
+
+    private Long totalTest;
+
+    private String context;
+
+    private Instant executionDate;
+
+    private String collection;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public Long getFailedTest() {
+        return failedTest;
+    }
+
+    public void setFailedTest(Long failedTest) {
+        this.failedTest = failedTest;
+    }
+
+    public Long getTotalTest() {
+        return totalTest;
+    }
+
+    public void setTotalTest(Long totalTest) {
+        this.totalTest = totalTest;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Instant getExecutionDate() {
+        return executionDate;
+    }
+
+    public void setExecutionDate(Instant executionDate) {
+        this.executionDate = executionDate;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "PostmanProcessor";
+    }
+}
