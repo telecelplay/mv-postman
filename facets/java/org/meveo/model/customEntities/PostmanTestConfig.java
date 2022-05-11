98e400a1f1cc97ad0d5ad7bda5467702bdb80801
babardev <babar.virk@gmail.com>
2022-05-11 20:29:30 +0000

    Add the cet java source file : PostmanTestConfig.javain the module : mv-postman

---
+++ facets/java/org/meveo/model/customEntities/PostmanTestConfig.java
@@ -0,0 +1,94 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class PostmanTestConfig implements CustomEntity {
+
+    public PostmanTestConfig() {
+    }
+
+    public PostmanTestConfig(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private Long failedTest;
+
+    private Long totalTest;
+
+    private String collectionFilePath;
+
+    private String contextFilePath;
+
+    private Instant execuationDate;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public Long getFailedTest() {
+        return failedTest;
+    }
+
+    public void setFailedTest(Long failedTest) {
+        this.failedTest = failedTest;
+    }
+
+    public Long getTotalTest() {
+        return totalTest;
+    }
+
+    public void setTotalTest(Long totalTest) {
+        this.totalTest = totalTest;
+    }
+
+    public String getCollectionFilePath() {
+        return collectionFilePath;
+    }
+
+    public void setCollectionFilePath(String collectionFilePath) {
+        this.collectionFilePath = collectionFilePath;
+    }
+
+    public String getContextFilePath() {
+        return contextFilePath;
+    }
+
+    public void setContextFilePath(String contextFilePath) {
+        this.contextFilePath = contextFilePath;
+    }
+
+    public Instant getExecuationDate() {
+        return execuationDate;
+    }
+
+    public void setExecuationDate(Instant execuationDate) {
+        this.execuationDate = execuationDate;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "PostmanTestConfig";
+    }
+}
