1843d93b44a13a98ad9701a4e16127e647c05979
babardev <babar.virk@gmail.com>
2022-05-11 15:32:51 +0000

    Add the cet java source file : PostmanProcessor.javain the module : mv-postman

--- facets/java/org/meveo/model/customEntities/PostmanProcessor.java
+++ facets/java/org/meveo/model/customEntities/PostmanProcessor.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class PostmanProcessor implements CustomEntity {
@@ -20,16 +19,6 @@ public class PostmanProcessor implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Long failedTest;
-
-    private Long totalTest;
-
-    private String context;
-
-    private Instant executionDate;
-
-    private String collection;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -45,46 +34,6 @@ public class PostmanProcessor implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Long getFailedTest() {
-        return failedTest;
-    }
-
-    public void setFailedTest(Long failedTest) {
-        this.failedTest = failedTest;
-    }
-
-    public Long getTotalTest() {
-        return totalTest;
-    }
-
-    public void setTotalTest(Long totalTest) {
-        this.totalTest = totalTest;
-    }
-
-    public String getContext() {
-        return context;
-    }
-
-    public void setContext(String context) {
-        this.context = context;
-    }
-
-    public Instant getExecutionDate() {
-        return executionDate;
-    }
-
-    public void setExecutionDate(Instant executionDate) {
-        this.executionDate = executionDate;
-    }
-
-    public String getCollection() {
-        return collection;
-    }
-
-    public void setCollection(String collection) {
-        this.collection = collection;
     }
 
     @Override()
