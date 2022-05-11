bdfbc1c8b2d0a3d619576d666e8425ac41fa54b1
babardev <babar.virk@gmail.com>
2022-05-11 15:32:54 +0000

    Add the cet java source file : PostmanProcessor.javain the module : mv-postman

--- facets/java/org/meveo/model/customEntities/PostmanProcessor.java
+++ facets/java/org/meveo/model/customEntities/PostmanProcessor.java
@@ -19,6 +19,8 @@ public class PostmanProcessor implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String collection;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +36,14 @@ public class PostmanProcessor implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
     }
 
     @Override()
